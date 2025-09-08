cb9f54f07e50b367b765a647a126497c864da922
Marlon2132 <troffstas752@gmail.com>
2025-09-08 21:21:52 +0300

    The message about entering the hourly rate incorrectly has been changed

--- Lab1/Reporter/Reporter.cpp
+++ Lab1/Reporter/Reporter.cpp
@@ -7,19 +7,18 @@ using namespace std;
 
 int main(int arg_count, char* arg_value[]) {
     if (arg_count != 4) {
-        cerr << "Example of usage: Reporter.exe <binary_file_name> <report_file_name> <payment_per_hour_value>\n";
+        cerr << "Example of usage: Reporter.exe <binary_file_name> <report_file_name> <hourly_rate_value>\n";
         return 1;
     }
     
     if (atof(arg_value[3]) <= 0) {
-        cerr << "Payment value per hour should be > 0\n";
+        cerr << "Hourly rate value should be > 0\n";
         return 1;
     }
 
     const char* kBinfile = arg_value[1];
     const char* kTxtfile = arg_value[2];
     double rate = atof(arg_value[3]);
-
     ifstream ifs(kBinfile, ios::binary);
 
     if (!ifs) {
@@ -35,7 +34,6 @@ int main(int arg_count, char* arg_value[]) {
     }
 
     ofs << "Отчёт по файлу «" << kBinfile << "».\n";
-
     Employee e;
 
     //double payment;
