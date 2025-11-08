baa4c9fac04a63cf3050d20725d37cafda1e5946
Marlon2132 <troffstas752@gmail.com>
2025-11-08 20:09:12 +0300

    Implemented the Sender functionality

--- Lab4/Lab4/Sender.cpp
+++ Lab4/Lab4/Sender.cpp
@@ -1,4 +1,5 @@
 #include "utils/input_parsing.h"
+#include "utils/message_queue.h"
 
 #include <iostream>
 #include <string>
@@ -36,4 +37,46 @@ int main(int argc, char** argv) {
     }
 
     unsigned short user_input = static_cast<unsigned short>(stoi(temp_string));
+
+    if (user_input) {
+        MessageQueue queue(file_name, 0l, false);
+        MessageQueue::Message msg;
+
+        while (true) {
+            if (!user_input) {
+                cout << "Enter 0 to exit, otherwise enter a number of messages to send: ";
+                cin >> temp_string;
+
+                while (!CheckIfUnsignedShort(temp_string, false)) {
+                    cout << "Enter 0 to exit, otherwise enter a number of messages to send: ";
+                    cin >> temp_string;
+                }
+
+                user_input = static_cast<unsigned short>(stoi(temp_string));
+
+                if (!user_input) {
+                    break;
+                }
+            }
+
+            cout << "--> ";
+            cin >> temp_string;
+
+            if (temp_string.length() > 19) {
+                cout << "Message too long, will send only the first 19 characters!" << endl;
+            }
+
+            msg = MessageQueue::Message(temp_string, sender_num);
+
+            if (!queue.WEnqueue(msg, INFINITE)) {
+                cout << "Failed to wait for a free space in the message queue!" << endl;
+
+                return -1;
+            }
+
+            user_input--;
+        }
+    }
+
+    return 0;
 }
