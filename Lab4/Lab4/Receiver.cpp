aa9e90d4b2a51176d10217fb00b61950db36b547
Marlon2132 <troffstas752@gmail.com>
2025-11-08 14:23:15 +0300

    Input of initial data to the Receiver has been added

---
+++ Lab4/Lab4/Receiver.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    string temp_string;
+    string file_name;
+
+    cout << "Enter the binary file name:" << endl;
+    cin >> file_name;
+
+    cout << "Enter the number of message entries in the binary file:" << endl;
+    cin >> temp_string;
+}
