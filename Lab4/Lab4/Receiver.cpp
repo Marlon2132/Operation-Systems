c42a50cbc53cb7ad84a27fc5ea39789ef1043dad
Marlon2132 <troffstas752@gmail.com>
2025-11-08 18:49:34 +0300

    Implemented Receiver functionality

--- Lab4/Lab4/Receiver.cpp
+++ Lab4/Lab4/Receiver.cpp
@@ -41,4 +41,86 @@ int main(int argc, char** argv) {
 
     unsigned short number_of_senders;
     number_of_senders = static_cast<unsigned short>(stoi(temp_string));
+
+    HANDLE* senders_started_events = new HANDLE[number_of_senders];
+    STARTUPINFOA* senders_startup_info = new STARTUPINFOA[number_of_senders];
+    PROCESS_INFORMATION* senders_process_info = new PROCESS_INFORMATION[number_of_senders];
+
+    for (unsigned short i = 0; i < number_of_senders; i++) {
+        ZeroMemory(&senders_startup_info[i], sizeof(STARTUPINFOA));
+        senders_startup_info[i].cb = sizeof(STARTUPINFOA);
+        senders_started_events[i] = CreateEventA(NULL, TRUE, FALSE, (string("SENDER_") + to_string(i) + "_STARTED").c_str());
+
+        if (NULL == senders_started_events[i]) {
+            cout << "Failed to create start event for sender process " << i << endl;
+
+            return -1;
+        }
+
+        string cmd = "Sender.exe " + file_name + " " + to_string(i);
+
+        if (!CreateProcessA(NULL,
+            const_cast<char*>(cmd.c_str()),
+            NULL,
+            NULL,
+            FALSE,
+            CREATE_NEW_CONSOLE,
+            NULL,
+            NULL,
+            &senders_startup_info[i],
+            &senders_process_info[i])) {
+            cout << "Failed to create sender process " << i << endl;
+
+            return -1;
+        }
+    }
+
+    WaitForMultipleObjects(number_of_senders, senders_started_events, TRUE, INFINITE);
+
+    cout << "Enter 0 to exit, otherwise enter a number of messages to wait for: ";
+    cin >> temp_string;
+
+    while (!CheckIfUnsignedShort(temp_string, false)) {
+        cout << "Enter 0 to exit, otherwise enter a number of messages to wait for: ";
+        cin >> temp_string;
+    }
+
+    unsigned short user_input = static_cast<unsigned short>(stoi(temp_string));
+
+    if (user_input) {
+        MessageQueue::Message msg;
+        while (true) {
+            if (!user_input) {
+                cout << "Enter 0 to exit, otherwise enter a number of messages to wait for: ";
+                cin >> temp_string;
+
+                while (!CheckIfUnsignedShort(temp_string, false)) {
+                    cout << "Enter 0 to exit, otherwise enter a number of messages to wait for: ";
+                    cin >> temp_string;
+                }
+
+                user_input = static_cast<unsigned short>(stoi(temp_string));
+
+                if (!user_input) {
+                    break;
+                }
+            }
+
+            if (!queue.WDequeue(msg, INFINITE)) {
+                cout << "Failed to wait for a message!" << endl;
+
+                return -1;
+            }
+
+            cout << "MESSAGE FROM [ " << msg.author << " ]: " << msg.data << endl;
+
+            user_input--;
+        }
+    }
+
+    delete[] senders_started_events;
+    delete[] senders_startup_info;
+    delete[] senders_process_info;
+
+    return 0;
 }
