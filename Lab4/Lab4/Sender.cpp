05705bc28239f57ccea7d3dda782329384f82364
Marlon2132 <troffstas752@gmail.com>
2025-11-08 19:21:57 +0300

    Added user_input input

--- Lab4/Lab4/Sender.cpp
+++ Lab4/Lab4/Sender.cpp
@@ -13,4 +13,18 @@ int main(int argc, char** argv) {
 
     string file_name = argv[1];
     unsigned short sender_num = static_cast<unsigned short>(atoi(argv[2]));
+
+    HANDLE process_start_event = OpenEventA(EVENT_MODIFY_STATE | SYNCHRONIZE, FALSE, (string("SENDER_") + argv[2] + "_STARTED").c_str());
+
+    if (process_start_event == NULL) {
+        cout << "Failed to open start event!" << endl;
+
+        return -1;
+    }
+
+    SetEvent(process_start_event);
+
+    string temp_string;
+    cout << "Enter 0 to exit, otherwise enter a number of messages to send: ";
+    cin >> temp_string;
 }
