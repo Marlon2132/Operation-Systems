62a1517ce8ea16aab8e9ee43bb71d5d8467d715a
Marlon2132 <troffstas752@gmail.com>
2025-11-08 15:23:05 +0300

    Improved data entry processing

--- Lab4/Lab4/Receiver.cpp
+++ Lab4/Lab4/Receiver.cpp
@@ -1,16 +1,30 @@
-﻿#include <iostream>
+﻿#include "utils/input_parsing.h"
+
+#include <iostream>
 #include <string>
 #include <windows.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    string temp_string;
     string file_name;
-
     cout << "Enter the binary file name:" << endl;
     cin >> file_name;
 
+    while (!CheckFileName(file_name, "", false)) {
+        cout << "Enter the binary file name:" << endl;
+        cin >> file_name;
+    }
+
+    string temp_string;
     cout << "Enter the number of message entries in the binary file:" << endl;
     cin >> temp_string;
+
+    while (!CheckIfPositiveLong(temp_string, false)) {
+        cout << "Enter the number of message entries in the binary file:" << endl;
+        cin >> temp_string;
+    }
+
+    long number_of_entries;
+    number_of_entries = stol(temp_string);
 }
