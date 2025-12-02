96dae8ca1e5a2c5c511006550af804de3d3e33d5
Marlon2132 <troffstas752@gmail.com>
2025-12-02 16:53:06 +0300

    Server.cpp added

---
+++ Lab5/Lab5/Server.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Server starting..." << endl;
+
+    int n;
+    cout << "Enter number of employees to create: ";
+    cin >> n;
+}
